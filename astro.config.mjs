0cdc790ac2195cf3f3aed07204b064ea4db1e2a5
Joaquin Trujillo <joaquin.d.trujillo@gmail.com>
2026-08-27 11:42:21 -0300

    adapto portal a mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://joacocap11.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
